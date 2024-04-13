de0dcaf5ebe7841673818df64dfecf99bfac5ec2
Mayonesio <jara.mayonesio@gmail.com>
2024-04-13 15:48:32 +0200

    Backend_commit002_Token in login and logout

--- back/src/routes/auth.routes.js
+++ back/src/routes/auth.routes.js
@@ -1,10 +1,20 @@
-import {Router} from 'express';
-import {login, register} from '../controllers/auth.controller.js'
+import { Router } from "express";
+import {
+  login,
+  register,
+  logout,
+  profile,
+} from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
 
 const router = Router();
 
-router.post('/register', register);
+router.post("/register", register);
 
-router.post('/login', login);
+router.post("/login", login);
 
-export default router
+router.post("/logout", logout);
+
+router.get("/profile", authRequired, profile);
+
+export default router;
