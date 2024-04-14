f65677c2b1223c01a6e01081970d089914872f8d
Mayonesio <jara.mayonesio@gmail.com>
2024-04-14 21:57:22 +0200

    Backend_commit003_CRUD complete

--- back/src/routes/task.routes.js
+++ back/src/routes/task.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import {authRequired} from '../middlewares/validateToken.js'
+import { authRequired } from "../middlewares/validateToken.js";
+import {
+  getTask,
+  getTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+} from "../controllers/task.controller.js";
 
 const router = Router();
 
-router.get('/tasks', authRequired, (req, res)=> res.send('tasks'))
+router.get("/tasks", authRequired, getTasks);
+router.get("/tasks/:id", authRequired, getTask);
+router.post("/tasks", authRequired, createTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
+router.put("/tasks/:id", authRequired, updateTask);
 
-export default router
+export default router;
