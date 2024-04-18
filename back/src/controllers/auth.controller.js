e8617474bad505ebd169bd49f0e79705c55e28f2
Mayonesio <jara.mayonesio@gmail.com>
2024-04-18 14:28:45 +0200

    Frontend_commit009_register already validated

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const register = async (req, res) => {
 
     const userFound = await User.findOne({ email });
     if(userFound)
-    return res.status(400).json({ message: ["El correo ya esta en uso"] });
+    return res.status(400).json(["El correo ya esta en uso"] );
 
     const passwordHash = await bcrypt.hash(password, 10);
     const newUser = new User({
